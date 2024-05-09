39dbb4f16f78e9df7f50ea7209d74694f4773aea
alvin frieson <alvinfrierson@fuller.edu>
2024-05-09 15:20:07 -0700

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import { useState } from 'react';
+import Results from './components/Results';
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleUserInput(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <Header />
+      <UserInput onChange={handleUserInput} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
+    </main>
+  );
+}
+
+export default App;
